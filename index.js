6c63e8565de4de42da18043378fe697a3fa4ad12
KenzoSenna <kenzoandblade@gmail.com>
2025-11-24 11:13:03 -0400

    feat: adicionada uma tela de cadastro na qual é um carrossel que leva ao banco principal ao cadastrar usuário.
    * Adicionado get no js para criar usuário do html.

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+// // anexação do js com html do kenzo
+
+// const formulario = document.getElementById("forms")
+
+// formulario.addEventListener('submit', function (cadastro){
+//     cadastro.preventDefault
+
+//     const dadosFormulados = new FormData(formulario)
+
+//     const objetoPuro = Object.fromEntries(dadosFormulados.entries())
+
+//     const jsonString = JSON.stringify(objetoPuro, null, 2)
+
+
+
+// })
+document.getElementById('forms').addEventListener('submit', criarCliente);
+function criarCliente(event)
+{
+    
+    event.preventDefault()
+
+    const form = event.target
+
+    const dadosForm = new FormData(form)
+
+    const nome = dadosForm.get('nome')
+    
+    const idade = dadosForm.get('idade')
+
+    const agencia_id = dadosForm.get('agencia')
+
+    const clientado = new cliente(nome, idade, agencia_id)
+}
+
+// objetificação do hud
+
 class agencia {
     static countAgencia = 1;
     #id;
@@ -31,6 +68,8 @@ class movimentacao
         return this
     }
 
+
+
     
 }
 
@@ -41,8 +80,12 @@ class cliente {
     #agenciaId;
     movimentacoes = [];
  
-    get saldo(){
+    get getSaldo(){
         return this.SaldoCliente;
+    }
+
+    set setSaldo(valor){
+        this.saldoCliente += valor
     }
 
     get agencia(){
@@ -52,14 +95,15 @@ class cliente {
     get historico(){
         return this.movimentacoes;
     }
-
+''
     transferencia(valor, outro) 
     {
         if(this == outro) 
             return 'Voce não pode transferir para voce mesmo'
         if(this.saldoCliente >= valor) {
-            outro.saldoCliente += valor
-            this.saldoCliente -= valor
+            outro.setSaldo(valor)
+            this.setSaldo(-valor)
+           
             console.log(`${outro.nome} Recebeu ${valor} de ${this.nome}`)
 
             this.salvarHistorico(new movimentacao(this, -valor, outro))
@@ -71,7 +115,7 @@ class cliente {
     saque(valor)
     {
         if(this.SaldoCliente >= valor){
-            this.saldoCliente -= valor;
+            this.setSaldo(-valor);
             this.salvarHistorico(new movimentacao(this, -valor));
         }
         else console.log("Dinheiro insuficiente")  
@@ -79,7 +123,7 @@ class cliente {
 
     deposito(valor)
     {
-        this.saldoCliente += valor;
+        this.setSaldo(valor);
         this.salvarHistorico(new movimentacao(this, +valor));
     }
     
@@ -104,7 +148,7 @@ hudson = new cliente('hudson', 10, agencia1.agencia)
 arthur = new cliente('Artgur', 10, agencia1.agencia)
 
 hudson.transferencia(5, arthur)
-
+console.log(hudson.getSaldo)
 // const elemento = document.getElementById("navbar-brand");
 // elemento.style.color = "yellow";
 hudson.deposito(10)
